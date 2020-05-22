d086325708deb542bd3968d562e5d5013ff97a80
leejin21c <leejin21c@gmail.com>
2020-05-22 19:01:55 +0900

    Updates

---
+++ precache-manifest.5fde7f9c4af69d724c5637924b37f5fc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7abf15c9f3fc3addfd4733459946cfc0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e6bd812a51e20e42ae57",
+    "url": "/static/css/main.af038d8b.chunk.css"
+  },
+  {
+    "revision": "79907e5e853fc1fa7794",
+    "url": "/static/js/2.0285f39f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0285f39f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6bd812a51e20e42ae57",
+    "url": "/static/js/main.76741e52.chunk.js"
+  },
+  {
+    "revision": "52f8b573bb3b2c0ff873",
+    "url": "/static/js/runtime-main.592242c7.js"
+  }
+]);
